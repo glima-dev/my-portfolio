56650f4bce2487af7a116ac400dda504fcd86305
glima-dev <guilherme.lima.tec@live.com>
2023-05-29 18:43:43 -0300

    first commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Guilherme Lima",
+  githubUser: "glima-dev",
+  linkedinUser: "glima-dev",
+  whatsappNumber: "+5511974465317",
+  emailUser: "guilherme.lima.tec@live.com",
+  instagramUser: "guilherme.lima.tec",
+  facebookUser: "",
+};
